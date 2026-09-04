ac304df5ff9e019ec011675bd1c9ac11fcf20bf0
Michelle Pagran <michellepagran143@gmail.com>
2026-09-05 04:05:24 +0800

    Add InvalidIndex.cpp with out-of-bounds array access
    This code attempts to access an out-of-bounds index of the array, which will lead to undefined behavior.

---
+++ Laboratory 1 /InvalidIndex.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int myFavoriteNumbers[5] = {1, 27, 18, 2, 7};
+    int index = myFavoriteNumbers[5];
+
+    cout << "My most favorite number is " << index << endl;
+
+    return 0;
+}
